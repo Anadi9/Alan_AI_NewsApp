c447c11b7c08ffd7a6e642e8d01c90b6df04b2fc
Anadi9 <anadithakur99@gmail.com>
2021-11-09 20:45:47 +0530

    all done its a small amazing project

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import wordsToNumbers from 'words-to-numbers';
 import NewsCards from './components/NewsCards/NewsCards';
-
+import useStyles from './styles';
+import alan from './images/alan2.png'
+import alanPowered from './images/alan_powered.png'
 
 const alanKey = '3c585e128455bc34dab6a0fc36567ea12e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 function App() {
 
   const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+  const classes = useStyles();
 
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles }) => {
+      onCommand: ({ command, articles, number }) => {
         if (command === 'newHeadlines') {
           setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers(number, {fuzzy : true}) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > 20) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
         }
-      }
-    })
-  }, [])
+      },
+    });
+  }, []);
   
   return (
-    <div className="App">
-      <h1>Alan AI News Application</h1>
-      <NewsCards articles={newsArticles} />
+    <div>
+      <div className={classes.logoContainer}>
+        <img className={classes.alanLogo} alt="alan logo" src={alan}/>
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+        <img className={classes.alanpowerLogo} alt="alan logo" src={alanPowered}/>
     </div>
   );
 }
--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,13 +1,26 @@
 import React, { useState, useEffect, createRef } from 'react';
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import useStyles from './styles';
+import classNames from 'classnames';
 
-const NewsCard = ({article: {description, publishedAt, source, title, url, urlToImage}, i}) => {
+const NewsCard = ({article: {description, publishedAt, source, title, url, urlToImage}, i, activeArticle}) => {
     
     const classes = useStyles();
+    const [elRefs, setElRefs] = useState([]);
+    const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
+
+    useEffect(() => {
+        setElRefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
+    }, []);
+
+    useEffect(() => {
+        if (i === activeArticle && elRefs(activeArticle)) {
+            scrollToRef(elRefs[activeArticle]);
+        }
+    }, [i, activeArticle, elRefs]);
     
     return (
-        <Card className={classes.card}>
+        <Card ref={elRefs[i]} className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
             <CardActionArea hrf={url} target="_blank">
                 <CardMedia image={urlToImage} className={classes.media} />
                 <div className={classes.details}>
